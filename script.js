26ec57cb77f1b6530816f468d788690e8ff08b24
vinirlq7 <145574909+vinirlq7@users.noreply.github.com>
2025-04-01 08:22:51 -0300

    01/04

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ document.addEventListener('DOMContentLoaded', function(){
 
     let tamanhoAtualFonte = 1;
 
-    
-})
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte+= 0.1; 
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+});
