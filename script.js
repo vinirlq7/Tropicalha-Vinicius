00f255c2fb1a2d10086367222e2eb2626c936c9a
vinirlq7 <145574909+vinirlq7@users.noreply.github.com>
2025-04-15 08:27:12 -0300

    15/04

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){
+    
+    const botaoDeAcessibilidade =document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = this.document.getElementById('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click', function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+    })
+
+
+    
     const aumentaFonteBotao = document .getElementById('aumentar-fonte');
     const diminuiFonteBotao = document .getElementById('diminuir-fonte');
     
     let tamanhoAtualFonte = 1;
-
     
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte+= 0.1; 
