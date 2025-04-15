c515b2fb08853c016bbc335442d31ef54b6becde
vinirlq7 <145574909+vinirlq7@users.noreply.github.com>
2025-04-15 08:32:23 -0300

    .

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade =document.getElementById('botao-acessibilidade');
     const opcoesDeAcessibilidade = this.document.getElementById('opcoes-acessibilidade');
 
-    botaoDeAcessibilidade.addEventListener('click', function(){
+    botaoDeAcessibilidade.addEventListener('click',function(){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
     })
 
 
